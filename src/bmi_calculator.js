0bfa88d9b9c4e8215793e563256b08eda583c1b2
lollypop27 <lara@adafctory.co.za>
2016-06-21 21:00:06 +0200

    adding BMI return messages

--- src/bmi_calculator.js
+++ src/bmi_calculator.js
@@ -9,3 +9,15 @@ BMICalculator.prototype.metric_bmi  = function(obj) {
     obj.bmiValue = parseFloat(finalBmi.toFixed(2));
   }
 };
+
+function setBMIMessage (obj, value){
+  if (obj.bmiValue < 18.5) {
+    obj.bmiMessage = "Underweight"
+  }
+  if (obj.bmiValue > 18.5 && obj.bmiValue < 25) {
+    obj.bmiMessage = "Normal"
+  }
+  if (obj.bmiValue > 25 && obj.bmiValue < 30) {
+    obj.bmiMessage = "Obese"
+  }
+};
