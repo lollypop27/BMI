1e76277d4a6fb72930509cb8f17c028fb01e7112
lollypop27 <lara@adafctory.co.za>
2016-06-21 20:02:43 +0200

    adding code to bmi calculator and updating html file to include bmi calculator src file

--- spec/bmi_calculator_spec.js
+++ spec/bmi_calculator_spec.js
@@ -1,8 +1,14 @@
-decriber("BMICalculator", function() {
+describe("BMICalculator", function() {
   var bmi_calculator;
   var person;
 
-  berforEach(function() {
-    person = new Person({weight:})
-  })
-}
+  beforEach(function() {
+    person = new Person({weight: 90, height: 186});
+    calculator = new BMICalculator();
+  });
+
+  it("calculates BMI for a person using metric method", function() {
+    calculator.metric_bmi(person);
+    expect(person.bmiValue).toEqual(26.01);
+  });
+});
