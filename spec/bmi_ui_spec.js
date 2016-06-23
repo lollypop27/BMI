0da8322e0ee613476a1e364313f1562b1abdef15
lollypop27 <lara@adafctory.co.za>
2016-06-23 21:39:35 +0200

    updating ui spec with imperial method

--- spec/bmi_ui_spec.js
+++ spec/bmi_ui_spec.js
@@ -1,18 +1,36 @@
 describe('BMI_UI - index.html', function() {
+  describe("Metric", function() {
     beforeEach(function() {
         jasmine.getFixtures().fixturesPath = '.';
         loadFixtures('index.html');
         $.holdReady(false);
+        $('#metric').prop("checked", true)
         $('#weight').val('90');
         $('#height').val('186');
         $('#calculate').trigger('click');
     });
-
     it("displays BMI Value", function() {
         expect($('#display_value').text()).toBe('Your BMI is 26.01');
     });
-
     it("displays BMI Message", function() {
         expect($('#display_message').text()).toBe('and you are Overweight');
     });
+})
+  describe("Imperial", function() {
+    beforeEach(function() {
+        jasmine.getFixtures().fixturesPath = '.';
+        loadFixtures('index.html');
+        $.holdReady(false);
+        $('#imperial').prop("checked", true)
+        $('#weight').val('198');
+        $('#height').val('73');
+        $('#calculate').trigger('click');
+    });
+    it("displays BMI Value", function() {
+        expect($('#display_value').text()).toBe('Your BMI is 26.12');
+    });
+    it("displays BMI Message", function() {
+        expect($('#display_message').text()).toBe('and you are Overweight');
+    });
+  })
 });
