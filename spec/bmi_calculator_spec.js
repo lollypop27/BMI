a587d8fed2a8b2a427fdf2dc4101502bbf9b44e9
lollypop27 <lara@adafctory.co.za>
2016-06-23 21:35:24 +0200

    updating calculator spec with imperial method

--- spec/bmi_calculator_spec.js
+++ spec/bmi_calculator_spec.js
@@ -1,15 +1,24 @@
 describe("BMICalculator", function() {
   var bmi_calculator;
   var person;
-
+describe("Metric method", function() {
   beforeEach(function() {
     person = new Person({weight: 90, height: 186});
     calculator = new BMICalculator();
   });
-
   it("calculates BMI for a person using metric method", function() {
     calculator.metric_bmi(person);
     expect(person.bmiValue).toEqual(26.01);
   });
-
+})
+describe("Imperial method", function() {
+  beforeEach(function() {
+    person = new Person({weight: 198, height: 73});
+    calculator = new BMICalculator();
+  });
+  it("calculates BMI for a person using Imperial method", function() {
+    calculator.imperial_bmi(person);
+    expect(person.bmiValue).toEqual(26.12);
+  });
+})
 });
